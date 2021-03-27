20688845017f18ff6c14f633156104773bfb4c74
HC <1264359108@qq.com>
2021-03-27 11:48:51 +0800

    commit 15

--- src/main/java/com/wyq/Commit2.java
+++ src/main/java/com/wyq/Commit2.java
@@ -3,12 +3,5 @@ package com.wyq;
 public class Commit2 {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("commit 4");
-        System.out.println("commit 5");
-        System.out.println("commit 6");
-        System.out.println("commit 7");
-        System.out.println("commit 8");
-        System.out.println("commit 14");
     }
 }
